ea575344d8100802d303031985cd16296b35acc9
Adrien LECOMTE <97622219+ZdarkBlackShadow@users.noreply.github.com>
2025-02-04 11:12:03 +0100

    add the calendar example

--- service/structure.go
+++ service/structure.go
@@ -14,19 +14,11 @@ type Epreuve struct {
 	Date            string `json:"when"`
 }
 
-type EpreuveStruct struct {
-	Id                  int    `json:"id"`
-	Name                string `json:"name"`
-	Description         string `json:"description"`
-	LogiqueAndReflexion string
-	Resultat            string
-	LearningLesson      []string
-}
-
 type User struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Id          int    `json:"id"`
+	Name            string `json:"name"`
+	Description     string `json:"description"`
+	Id              int    `json:"id"`
+	RoleDescription string `json:"roleDescription"`
 }
 type UserStruct struct {
 	List []string `json:"list"`
@@ -34,19 +26,38 @@ type UserStruct struct {
 }
 
 type Exercice struct {
-	ID              int    `json:"id"`
-	Name            string `json:"name"`
-	Description     string `json:"description"`
-	Image           string `json:"image"`
-	CodeGo          string `json:"codeGo"`
-	CodeHTML        string `json:"codehtml"`
-	CodeCSS         string `json:"codecss"`
-	FinalImage      string `json:"finalresult"`
-	FinalResultCode string `json:"finalResultCode"`
-	Timer           int    `json:"timerInMinute"`
-	When            string `json:"when"`
+	ID               int    `json:"id"`
+	Name             string `json:"name"`
+	Description      string `json:"description"`
+	Image            string `json:"image"`
+	ExempleRenduCode string `json:"ExempleRendu"`
+	CodeGo           string `json:"codeGo"`
+	CodeHTML         string `json:"codehtml"`
+	CodeCSS          string `json:"codecss"`
+	FinalImage       string `json:"finalresult"`
+	FinalResultCode  string `json:"finalResultCode"`
+	Timer            int    `json:"timerInMinute"`
+	When             string `json:"when"`
 }
 
 type DataTDB struct {
 	Datatdb string
+}
+
+//Structure pour le calendrier
+
+// Structure pour représenter un événement
+type Event struct {
+	Heure       string `json:"heure"`
+	Titre       string `json:"titre"`
+	Description string `json:"description"`
+}
+
+// Structure pour représenter les événements de chaque jour
+type WeekSchedule struct {
+	Lundi    []Event `json:"lundi"`
+	Mardi    []Event `json:"mardi"`
+	Mercredi []Event `json:"mercredi"`
+	Jeudi    []Event `json:"jeudi"`
+	Vendredi []Event `json:"vendredi"`
 }
