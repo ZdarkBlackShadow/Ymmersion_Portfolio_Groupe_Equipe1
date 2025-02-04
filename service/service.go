26e7e7237277ab95160c3a0b854f0277ee9a32cd
Mirkium <alexandre.petitfrere@ynov.com>
2025-02-04 11:45:38 +0100

    init img user json

--- service/service.go
+++ service/service.go
@@ -23,7 +23,6 @@ func InitServer() {
 	http.HandleFunc("/exemple", Exemple)
 	http.HandleFunc("/tableaudebord", Tableaudebord)
 	http.HandleFunc("/adrien", EpreuveAdrien)
-	http.HandleFunc("/tableau_epreuve", PageEpreuve)
 	http.HandleFunc("/alexandre", EpreuveAlexandre)
 	fileserver := http.FileServer(http.Dir("./assets"))
 	http.Handle("/static/", http.StripPrefix("/static/", fileserver))
